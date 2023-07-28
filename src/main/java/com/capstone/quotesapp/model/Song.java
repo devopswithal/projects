32e63ff17ec114f7686658fb11d10adc0cb11bbd
devopswithal <devopswithalprojects@gmail.com>
2023-07-28 14:26:02 -0500

    Added Song Controller, Model, & Repository

---
+++ src/main/java/com/capstone/quotesapp/model/Song.java
@@ -0,0 +1,28 @@
+package com.capstone.quotesapp.model;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "songs")
+public class Song {
+    public Long getId() {
+        return id;
+    }
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "songID")
+    private Long id;
+
+    public String getSong() {
+        return so;
+    }
+
+    public void setSong(String song) {
+        this.song = song;
+    }
+
+    @Column(name = "song_string")
+    private String song;
+
+}
